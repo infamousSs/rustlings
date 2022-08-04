ce873c0534cd9fc57978eca584bbaacb4a0daded
LukeD <locdaohuuit@outlook.com>
2022-08-04 01:10:08 -0700

    day#6

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -14,18 +14,57 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub struct ReportCard {
     pub grade: f32,
     pub student_name: String,
     pub student_age: u8,
+    pub use_grade_as_string: bool,
+}
+
+trait Grade {
+    fn as_number(&self) -> String;
+    fn as_string(&self) -> String;
+}
+
+impl Grade for f32 {
+    fn as_number(&self) -> String {
+        self.to_string()
+    }
+
+    fn as_string(&self) -> String {
+        match self {
+            -1.0..=1.0 => "F-".into(),
+            1.0..=1.5 => "F".into(),
+            1.5..=2.0 => "D".into(),
+            2.0..=2.5 => "C".into(),
+            2.5..=3.0 => "C+".into(),
+            3.0..=3.5 => "B".into(),
+            4.0..=4.5 => "B+".into(),
+            4.5..=5.0 => "A".into(),
+            5.0..=5.5 => "A+".into(),
+            x if x > &5.5 => "A+".into(),
+            x => "Invalid".into(),
+        }
+    }
 }
 
 impl ReportCard {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        if self.use_grade_as_string {
+            format!(
+                "{} ({}) - achieved a grade of {}",
+                &self.student_name,
+                &self.student_age,
+                &self.grade.as_string()
+            )
+        } else {
+            format!(
+                "{} ({}) - achieved a grade of {}",
+                &self.student_name,
+                &self.student_age,
+                &self.grade.as_number()
+            )
+        }
     }
 }
 
@@ -39,6 +78,7 @@ mod tests {
             grade: 2.1,
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
+            use_grade_as_string: false,
         };
         assert_eq!(
             report_card.print(),
@@ -50,9 +90,10 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: 5.5,
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
+            use_grade_as_string: true,
         };
         assert_eq!(
             report_card.print(),
--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -3,8 +3,6 @@
 // can offer. Follow the steps to complete the exercise.
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
@@ -12,7 +10,7 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => first.to_string().to_uppercase() + c.as_str(),
     }
 }
 
@@ -21,7 +19,12 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    // let mut res = Vec::new();
+    // for word in words.iter() {
+    //     res.push(capitalize_first(word));
+    // }
+    // res
+    words.iter().map(|word| capitalize_first(word)).collect()
 }
 
 // Step 3.
@@ -29,7 +32,15 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    // let mut res = String::from("");
+    // for word in words.iter() {
+    //     res.push_str(&capitalize_first(&word));
+    // }
+    // res
+    words
+        .iter()
+        .map(|word| capitalize_first(word))
+        .collect::<String>()
 }
 
 #[cfg(test)]
